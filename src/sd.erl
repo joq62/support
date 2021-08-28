db7bbaf71cdf1b9d23f6c983ecdcee3ee3e69482
joq62 <joakim.leche@gmail.com>
2021-08-29 00:29:57 +0200

    added call and cast in sd

--- src/sd.erl
+++ src/sd.erl
@@ -27,6 +27,34 @@
 %% Description: Initiate the eunit tests, set upp needed processes etc
 %% Returns: non
 %% --------------------------------------------------------------------
+call(App,M,F,A,T)->
+    Result=case sd:get(App) of
+	       []->
+		   {error,[eexists,App,?FUNCTION_NAME,?MODULE,?LINE]};
+	       [Node|_]->
+		   rpc:call(Node,M,F,A,T)
+	   end,
+    Result.
+
+	%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+cast(App,M,F,A)->
+    Result=case sd:get(App) of
+	       []->
+		   {error,[eexists,App,?FUNCTION_NAME,?MODULE,?LINE]};
+	       [Node|_]->
+		   rpc:cast(Node,M,F,A)
+	   end,
+    Result.			   
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
 all()->
     Apps=[{Node,rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
     AvailableNodes=[{Node,AppList}||{Node,AppList}<-Apps],
